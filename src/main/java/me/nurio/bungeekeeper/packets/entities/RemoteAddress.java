ccafa2cbc03a7511453660826e5b17770dc942c3
xXNurioXx <xxnurioxx@gmail.com>
2020-05-30 13:46:06 +0200

    Added methods to RemoteAddress

--- src/main/java/me/nurio/bungeekeeper/packets/entities/RemoteAddress.java
+++ src/main/java/me/nurio/bungeekeeper/packets/entities/RemoteAddress.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
 @Getter
 @AllArgsConstructor
 @RequiredArgsConstructor
@@ -13,5 +16,20 @@ public class RemoteAddress {
     @NonNull private String ipAddress;
     @NonNull private String hostName;
     private int port;
+
+    public static RemoteAddress fromInetSocket(InetSocketAddress address) {
+        return new RemoteAddress(
+            address.getAddress().getHostAddress(),
+            address.getAddress().getHostName(),
+            address.getPort()
+        );
+    }
+
+    public static RemoteAddress fromInetAddress(InetAddress address) {
+        return new RemoteAddress(
+            address.getHostAddress(),
+            address.getHostName()
+        );
+    }
 
 }
